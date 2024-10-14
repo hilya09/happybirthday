d6eabc8d0fcb05809fe6e3fae94fa96480de619e
hilya09 <hilyaanbiyani09@gmail.com>
2024-10-14 20:18:36 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,21 @@
+# =[Modules dan Packages]========================
+
+from flask import Flask,render_template,request,jsonify
+
+# =[Variabel Global]=============================
+
+app = Flask(__name__, static_url_path='/static')
+model = None
+
+# =[Routing]=====================================
+
+# [Routing untuk Halaman Utama atau Home]
+@app.route("/")
+def beranda():
+    return render_template('index.html')
+
+# =[Main]========================================
+if __name__ == '__main__':
+
+    # Run Flask di localhost
+    app.run(host="localhost", port=5000, debug=True)
